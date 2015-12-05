f429ad9758b60d8e6800dca28d95cc30ad3ab1d9
suyhmedu <suyhmed@gmail.com>
2015-12-05 13:55:32 -0500

    Started

---
+++ Lawn Lovers/signInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  signInViewController.swift
+//  Lawn Lovers
+//
+//  Created by Suyaib Ahmed on 12/5/15.
+//  Copyright © 2015 LawnLovers. All rights reserved.
+//
+
+import UIKit
+
+class signInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
